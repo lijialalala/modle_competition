15865dddb4cd3479df370e2821d1f1e935cffc4c
lijia <810932574@qq.com>
2016-04-16 18:16:15 +0800

    修改些bug

--- vote.php
+++ vote.php
@@ -33,7 +33,7 @@ function fetchAll($sql,$result_type=MYSQL_ASSOC){
     <link type="text/css" rel="stylesheet" href="css/vote.css">
     <link type="text/css" rel="stylesheet" href="css/style.css">
     <script type="text/javascript" src="js/jquery.js"></script>
-    <script type="text/javascript" src="js/script.js"></script>
+    <script type="text/javascript" src="js/index.js"></script>
     <script type="text/javascript" src="js/jquery.min.js"></script>
     <script type="text/javascript">
         $(document).ready(function () {
